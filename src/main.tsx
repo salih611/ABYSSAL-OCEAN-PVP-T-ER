1abb97f9b88a6ebdbfbe77dc71479d212d3750ef
salih Başaran <salihoktaybasaran54@gmail.com>
2026-06-08 20:32:28 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import AdminPanel from "./AdminPanel";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+// Basit routing
+const path = window.location.pathname;
+const Component = path === "/admin" ? AdminPanel : App;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Component />
+  </React.StrictMode>
+);
