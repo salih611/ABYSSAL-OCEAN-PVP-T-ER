729a2b3bffcfdd0c4c844c5e751f0ae0cd8de3f0
salih Başaran <salihoktaybasaran54@gmail.com>
2026-06-08 20:34:02 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,23 @@ import App from "./App";
 import AdminPanel from "./AdminPanel";
 import "./index.css";
 
-// Basit routing
-const path = window.location.pathname;
-const Component = path === "/admin" ? AdminPanel : App;
+function Root() {
+  const [path, setPath] = React.useState(window.location.pathname);
+
+  React.useEffect(() => {
+    const handlePop = () => setPath(window.location.pathname);
+    window.addEventListener("popstate", handlePop);
+    return () => window.removeEventListener("popstate", handlePop);
+  }, []);
+
+  if (path === "/admin" || path === "/admin/") {
+    return <AdminPanel />;
+  }
+  return <App />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Component />
+    <Root />
   </React.StrictMode>
 );
